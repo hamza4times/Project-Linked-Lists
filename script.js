175cc64b548b31fd45ee17e1d94f4e83fefa818b
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-06-29 09:51:58 -0500

    create skull class structure

---
+++ script.js
@@ -0,0 +1,54 @@
+class LinkedList {
+    constructor(value){
+        this.value = value;
+    }
+
+    append(value){
+
+    }
+
+    prepend(value){
+
+    }
+
+    size(){
+
+    }
+
+    head(){
+
+    }
+
+    tail(){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    findIndex(value){
+
+    }
+
+    toString(){
+        
+    }
+}
+
+class Node {
+
+    constructor(value, nextNode){
+        this.value = null;
+        this.nextNode = null;
+    }
+
+}
